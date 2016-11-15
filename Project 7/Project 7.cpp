6a2b50ba9b8da04539eccedd9d08c07730bb52bc
Inteligirl <mischieval@gmail.com>
2016-11-15 06:17:49 -0500

    entered a loop to ask user to retry

--- Project 7/Project 7.cpp
+++ Project 7/Project 7.cpp
@@ -2,6 +2,7 @@
 #include<string>
 
 
+
 using namespace std;
 
 
@@ -57,9 +58,7 @@ space in the string to uppercase letters.
 ***********************************************/
 void CharConverter::properWords(string s)
 {
-	//int len = s.length();	//get length of the string function. its returning a int and storing in len.
-		
-		for (int i = 0; i < s.length(); i++) //for loop to check if the first character is either lower or upper get convert it
+	for (int i = 0; i < s.length(); i++) //for loop to check if the first character is either lower or upper get convert it
 		{
 			if (i == 0)
 			{
@@ -103,35 +102,48 @@ void formatdash()
 int main()
 {
 	CharConverter cc;
-	int choice;
+	int menuChoice;
 	string s;
+	string ch;
 	
 
 	welcome();	//call welcome function
 
-	cout << "User please enter a string : ";	//user decides function method to call
-	getline(cin, s);
-	cout << "\nUser choose which method to use on the string you entered.\n" << endl;
-	cout << "Choose 1 to uppercase the entire string." << endl;
-	cout << "Choose 2 to uppercase the first letter of each word." << endl;
-	cout << "Choose 3 to exit the program." << endl;
-	cout << "\nEnter your choice : ";
+	while (ch != "n" && ch != "N")
+	{	
 
-	cin >> choice;
+		cout << "User please enter a string : ";	//user decides function method to call
 
-	formatdash();
-		
-	// switch case to select the one of two choices
-	switch (choice)		//switch used to call appropriate function
-	{
-	case 1: cc.uppercase(s);	//calls uppercase function method of object cc in class CharConverter. passing argument s.
-		break;
-	case 2: cc.properWords(s);	//calls properWords function method of object cc in class CharConverter. passing argument s.
-		break;
-	case 3: cout << "\nYou have chosen to exit. Goodbye.\n" << endl;
-		exit(0);
-	default: cout << "\nInvalid selection." << "\nPlease choose either 1, 2, or 3." << endl;
+		getline(cin, s);
+
+		cout << "\nUser choose which method to use on the string you entered.\n" << endl;
+		cout << "Choose 1 to uppercase the entire string." << endl;
+		cout << "Choose 2 to uppercase the first letter of each word." << endl;
+		cout << "Choose 3 to exit the program." << endl;
+		cout << "\nEnter your choice : ";
+
+		cin >> menuChoice;
+
+		formatdash();
+
+		// switch case to select the one of two choices
+		switch (menuChoice)		//switch used to call appropriate function
+		{
+		case 1: cc.uppercase(s);	//calls uppercase function method of object cc in class CharConverter. passing argument s.
+			break;
+		case 2: cc.properWords(s);	//calls properWords function method of object cc in class CharConverter. passing argument s.
+			break;
+		case 3: cout << "\nYou have chosen to exit. Goodbye.\n" << endl;
+			exit(0);
+		default: cout << "\nInvalid selection." << "\nPlease choose either 1, 2, or 3." << endl;
+		}
+
+		cout << "\nWould you like to enter another string?\n";	// user decides to run program again
+		cout << "\nEnter 'Y' to continue or 'N' to exit: ";
+
+		cin >> ch;
+		cin.ignore();
 	}
-
+	
 	return 0;
 }
