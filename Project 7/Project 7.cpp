f232cd67cd954b13388ba1c9f8ff82195f8abd29
Inteligirl <mischieval@gmail.com>
2016-11-15 06:45:22 -0500

    final

--- Project 7/Project 7.cpp
+++ Project 7/Project 7.cpp
@@ -35,19 +35,7 @@ void CharConverter::uppercase(string s)
 {
 	for (int i = 0; i < s.length(); i++)
 	{
-		//valid check for first letter is either 0 to 9 or space then left //WTF IS THIS COULD BE SHORTER?
-		if (s.at(i) == ' ' || s.at(i) == '0' ||
-			s.at(i) == '1' || s.at(i) == '2' ||
-			s.at(i) == '3' || s.at(i) == '4' ||
-			s.at(i) == '5' || s.at(i) == '6' ||
-			s.at(i) == '7' || s.at(i) == '8' ||
-			s.at(i) == '9' || s.at(i) > 'A' && s.at(i) < 'Z')
-			continue; 		//what is this???
-		else
-		{
-			//convert to capital
-			s[i] = s.at(i) - 'a' + 'A'; 	//is this in the book?
-		}
+		s[i] = toupper(s[i]);
 	}
 	cout << "\nYour string in all uppercase is:\n " << endl << s << endl << endl;
 }
@@ -112,7 +100,7 @@ int main()
 	while (ch != "n" && ch != "N")
 	{	
 
-		cout << "User please enter a string : ";	//user decides function method to call
+		cout << endl << "User please enter a string : ";	//user decides function method to call
 
 		getline(cin, s);
 
@@ -137,6 +125,8 @@ int main()
 			exit(0);
 		default: cout << "\nInvalid selection." << "\nPlease choose either 1, 2, or 3." << endl;
 		}
+
+		formatdash();
 
 		cout << "\nWould you like to enter another string?\n";	// user decides to run program again
 		cout << "\nEnter 'Y' to continue or 'N' to exit: ";
